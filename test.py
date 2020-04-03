e33bef4c2b5c301d6043fb5cb69b600d80370ab0
zhengow <38389586+zhengow@users.noreply.github.com>
2020-04-03 16:16:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ import pandas as pd
 
 df = pd.read_csv('stkindex.csv',index_col=['Unnamed: 0'])
 print(df.iloc[0:5,0:5])
+
+BAIDUID=4FBBBA3328A3E9061F3B3061F22E109C:FG=1; BIDUPSID=4FBBBA3328A3E9061F3B3061F22E109C; PSTM=1585876305; BD_UPN=12314753; bdindexid=oigl20icm8r2i5rmga7kl4cn54; indexpro=4os6ije9bhv4ndqiqljf5fdla6; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; H_PS_645EC=0308ybfadt3GBTu0xhggM2H%2BLHSd%2F5G8%2B3m7%2BsHa9%2FYUdmbZVKSeAWyu6iYWViOMHQ1z; delPer=0; BD_CK_SAM=1; PSINO=6; BD_HOME=1; H_PS_PSSID=30962_1426_31118_21119_31186_30824_26350_31164; RT="sl=0&ss=k8jwsigq&tt=0&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&z=1&dm=baidu.com&si=16nb13whi31&ul=3968&hd=396b
